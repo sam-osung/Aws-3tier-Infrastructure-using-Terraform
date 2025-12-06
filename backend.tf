3e99643deafdd3b9381c2ec54c02a918c4d043d5
sammy-osung <osung996@gmail.com>
2025-12-06 13:48:58 +0100

    terraform apply

--- backend.tf
+++ backend.tf
@@ -7,7 +7,7 @@ terraform {
   backend "s3" {
     bucket  = "sammyterraformbucket"          # replace with your S3 bucket name
     key     = "bankapp/terraform.tfstate"     # path/file inside the bucket
-    region  = "us-east-1"                     # your AWS region
+    region  = "us-east-1"                     # your AWS region 
     encrypt = true
   }
 }
